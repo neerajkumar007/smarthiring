670bf47d42cafe92f43c87c9963596028e5b5b91
Neeraj Kumar <neeraj.kumar@employwise.com>
2021-06-04 00:35:04 +0530

    First commit

---
+++ src/main/java/com/nk/smarthiring/SmarthiringApplication.java
@@ -0,0 +1,13 @@
+package com.nk.smarthiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthiringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nk/smarthiring/SmarthiringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nk.smarthiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthiringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
